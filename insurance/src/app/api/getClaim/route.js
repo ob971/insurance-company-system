ef5cfe6e9e342467fe64ea76c3651bf3f2956a8c
Tamiru-Alemnew <tamirualemnew33@gmail.com>
2024-01-06 10:54:17 +0300

    populate the database with sample data

--- insurance/src/app/api/getClaim/route.js
+++ insurance/src/app/api/getClaim/route.js
@@ -4,7 +4,7 @@ import Claim from "../../../models/claims";
 
 export async function GET(req) {
   await connectDB();
-  const { userId } = req.query;
-  const payments = await Claim.find({ userId });
+//   const { userId } = req.query;
+  const payments = await Claim.find({ userId: "6598fb57804b9630c26887bc" });
   return new NextResponse().json(payments);
 }
